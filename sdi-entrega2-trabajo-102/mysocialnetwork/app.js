e4e316f784e0ff974cdb89c30deee729217f078f
Pablo García Fernández <uo276824@uniovi.es>
2022-04-30 13:09:47 +0200

    Register, Login y Logout [INCOMPLETO].

--- sdi-entrega2-trabajo-102/mysocialnetwork/app.js
+++ sdi-entrega2-trabajo-102/mysocialnetwork/app.js
@@ -6,37 +6,44 @@ const logger = require('morgan');
 const database = require('./database.js')
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const usersRepository = require("./repositories/usersRepository.js");
 
 let app = express();
+const {MongoClient} = require("mongoose");
 database.connect();
+let crypto = require('crypto');
+let expressSession = require('express-session');
+app.use(expressSession({secret: 'abcdefg', resave: true, saveUninitialized: true}));
+app.set('clave', 'abcdefg');
+app.set('crypto', crypto);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'twig');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+usersRepository.init(app, MongoClient);
+require("./routes/users.js")(app, usersRepository);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
--- sdi-entrega2-trabajo-102/mysocialnetwork/routes/users.js
+++ sdi-entrega2-trabajo-102/mysocialnetwork/routes/users.js
@@ -1,9 +1,65 @@
-var express = require('express');
-var router = express.Router();
+module.exports = function (app, usersRepository) {
+    app.get('/users', function (req, res) {
+        res.send('lista de usuarios');
+    })
+    app.get('/users/signup', function (req, res) {
+        res.render("signup.twig");
+    })
+    app.get('/users/login', function (req, res) {
+        res.render("login.twig");
+    })
+    app.post('/users/login', function (req, res) {
+            let securePassword = app.get("crypto").createHmac('sha256', app.get('clave'))
+                .update(req.body.password).digest('hex');
+            let filter = {
+                email: req.body.email,
+                password: securePassword
+            }
+            let options = {};
+            usersRepository.findUser(filter, options).then(user => {
+                if (user == null) {
+                    req.session.user = null;
+                    //res.send("Usuario no identificado");
+                    res.redirect("/users/login" + "?message=Email o password incorrecto" + "&messageType=alert-danger ");
+                } else {
+                    req.session.user = user.email;
+                    res.redirect("/publications");
+                }
+            }).catch(error => {
+                req.session.user = null;
+                //res.send("Se ha producido un error al buscar el usuario: " + error)
+                res.redirect("/users/login" + "?message=Se ha producido un error al buscar el usuario" + "&messageType=alert-danger ");
+            })
+        }
+    )
+    app.get('/users/logout', function (req, res) {
+        req.session.user = null;
+        res.send("El usuario se ha desconectado correctamente");
+    })
+    app.post('/users/signup', function (req, res) {
+        let securePassword = app.get("crypto").createHmac('sha256', app.get('clave'))
+            .update(req.body.password).digest('hex');
+        let repeatSecurePassword = app.get("crypto").createHmac('sha256', app.get('clave'))
+            .update(req.body.passwordCheck).digest('hex');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
-module.exports = router;
+        if (securePassword === repeatSecurePassword) {
+            let user = {
+                email: req.body.email,
+                name: req.body.name,
+                surname: req.body.surname,
+                password: securePassword
+            }
+            usersRepository.insertUser(user).then(userId => {
+                res.redirect("/users/login" + '?message=Nuevo usuario registrado.' +
+                    "&messageType=alert-info");
+            }).catch(error => {
+                res.redirect("/users/signup" + '?message=Se ha producido un error al registrar el usuario.' +
+                    "&messageType=alert-danger");
+            });
+        } else {
+            //Igual esto se podía modificar para q no se pierda todo lo demás q puso en el registro
+            res.redirect("/users/signup" + '?message=Ambas contraseñas deben de ser iguales.' +
+                "&messageType=alert-danger");
+        }
+    });
+}
