a3f1b746752f36b8b1a738552727397bd649ac27
uo277963 <uo277963@uniovi.es>
2022-05-05 11:59:51 +0200

    posts se crean y añaden

--- sdi-entrega2-trabajo-102/mysocialnetwork/app.js
+++ sdi-entrega2-trabajo-102/mysocialnetwork/app.js
@@ -30,7 +30,7 @@ app.use("/admin", adminSessionRouter);
 const userSessionRouter = require("./routes/userSessionRouter");
 app.use("/users", userSessionRouter);
 app.use("/friends", userSessionRouter);
-app.use("/publications", userSessionRouter);
+app.use("/posts", userSessionRouter);
 
 let userModel = require("./schemas/schema").User
 let postModel = require("./schemas/schema").Post
@@ -38,7 +38,7 @@ const usersRepository = require("./repositories/usersRepository.js");
 usersRepository.init(app, userModel);
 require("./routes/users.js")(app, userModel, usersRepository);
 require("./routes/admin.js")(app, userModel);
-require("./routes/posts.js")(app, postModel)
+require("./routes/posts.js")(app, postModel, userModel);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- sdi-entrega2-trabajo-102/mysocialnetwork/routes/posts.js
+++ sdi-entrega2-trabajo-102/mysocialnetwork/routes/posts.js
@@ -1,9 +1,26 @@
-module.exports = function(app, postModel) {
+module.exports = function(app, postModel, userModel) {
+    app.get("/posts", function(req, res) {
+        res.redirect("/posts/add");
+    });
     app.get("/posts/add", function(req, res) {
         res.render("posts/add.twig");
     });
-    app.post("/posts/add", function(req, res) {
-        let post = {};
-
+    app.post("/posts/add", async function(req, res) {
+        let creatorUser;
+        await userModel.findOne({email: req.session.user}).exec(function (err, user) {
+            if (err) {
+                console.log(err);
+            } else {
+              creatorUser = user;
+                let postPlain = {
+                    title: req.body.title,
+                    body: req.body.body,
+                    owner: creatorUser
+                };
+                new postModel(postPlain).save();
+                res.redirect("/users/list"); // TODO: redirect a post list mejor
+            }
+        });
     });
+
 }
