00227f32d6ee83c9d3490cb1be030bcabc6cc802
ludvij <vijande48@gmail.com>
2022-05-07 02:29:28 +0200

    refactor

--- sdi-entrega2-trabajo-102/mysocialnetwork/routes/api/middleware/checkJWT.js
+++ sdi-entrega2-trabajo-102/mysocialnetwork/routes/api/middleware/checkJWT.js
@@ -1,5 +1,6 @@
 
 module.exports.checkJWT = (app) => {
+	// reused from my ASW lab group :3
 	return checker = (req, res, next) => {
 		// get the jwt token from the header
 		const token = req.headers.token
@@ -7,14 +8,13 @@ module.exports.checkJWT = (app) => {
 		try {
 			let JWTPayload = jwapp.get('jwt').verify(token, app.get('jwt_secret')) 
 			if(Date.now() >= JWTPayload.exp * 1000) {
-				res.status(401)
+				return res.status(401)
 					.header('WWW-Authenticate', 'Basic realm="session has ended, pelase log in again"')
 					.send("The token has expired, please log in again")
-				return;
 			}
 			res.locals.jwtPayload = JWTPayload; 
 		} catch (error) {
-			res.status(401)
+			return res.status(401)
 				.header('WWW-Authenticate', 'Basic realm="you need to be logged in"')
 				.send("an error has happened while authorizing");
 			return;
